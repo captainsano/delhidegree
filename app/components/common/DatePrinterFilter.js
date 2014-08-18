f240d38e17dab719ac9a293a3b1e992cce2593b1
Santhos Baala RS <rssanthosbaala@gmail.com>
2014-08-18 18:00:52 +0530

    Filter applied on bottom temperatures.

---
+++ app/components/common/DatePrinterFilter.js
@@ -0,0 +1,24 @@
+(function(angula, moment) {
+
+    'use strict';
+
+    /**
+     * Filter returns hour for given date string,
+     * at 00:00, returns the day+month label
+     * @param index - the index, if 0 will return hour
+     */
+    angular
+        .module('dd')
+        .filter('dateprinter', [
+            function() {
+                return function(input, index) {
+                    var d = moment(input);
+                    if (d.hours() === 0 || index === 0) {
+                        return d.format('MMM DD HH:mm');
+                    }
+                    return d.format('HH:mm');
+                };
+            }
+        ])
+
+}(angular, moment));
